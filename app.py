f94ab19bcd6124bfae521f50ffd7145be990982e
redanzo <66446416+redanzo@users.noreply.github.com>
2023-05-13 01:51:19 +0000

    mp3

--- app.py
+++ app.py
@@ -1,11 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, send_file
+import pytube
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-  return "Hello Red!"
+def index():
+    return render_template('index.html')
 
+@app.route('/download', methods=['POST'])
+def download():
+    # get the YouTube video link from the form
+    video_url = request.form['video_url']
+
+    # create a YouTube object and extract the audio stream
+    youtube = pytube.YouTube(video_url)
+    audio_stream = youtube.streams.filter(only_audio=True).first()
+
+    # set the filename to the video title
+    filename = youtube.title + ".mp3"
+
+    # download the audio stream as an MP3 file
+    audio_stream.download(output_path=".", filename=filename)
+
+    # return the MP3 file to the user for download
+    return send_file(filename, as_attachment=True)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
